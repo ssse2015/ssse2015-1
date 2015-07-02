1c90150beb89f2706ddae929d11b7a4e4f96d3be
unknown <SSSE2015@E6440_LEGO1.pace.edu>
2015-07-01 21:30:17 -0400

    a fact appears when you click the oyster!

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var facts=[
 	"Oysters are actually a name for a few types of clams and mollusks",
 	"A group of oysters is called a bed, colony or reef",
 	"They have super strong adductor muscles to pull their shells closed when threatened",
-	"Oysters are bivalves which essentially means it’s like an open close shell connected by a ligament",
+	"Oysters are bivalves which essentially means that they have an open close shell connected by a ligament",
 	"Oysters are keystone species in the NY harbor and they are functionally extinct due to pollution and over harvesting",
 	"BOP has grown 11.5 million oysters in the NY harbor",
 	"Oysters reproduce when they feel a change in water temperature or the biomass of phytoplankton increases",
-	"It only takes one oyster to set off the rest of them",
-	"They reproduce by releasing a Gonad which will be either sperm or eggs into the water",
+	"It only takes one oyster to start the reproduction process to make the rest of them join in",
+	"Oysters reproduce by releasing a Gonad which will be either sperm or eggs into the water",
 	"An egg must be fertilized by both an egg and a sperm in order to develop",
 	"Once an egg is fertilized it drifts away from spawning site due to water currents",
 	"Oyster larvae develop for about two weeks",
@@ -23,9 +23,7 @@ var facts=[
 	
 
 
-$(document).ready(function(){
-	var fact=Math.round(Math.random()*(0-18)*-1);
-	var pickedFact=facts[fact];
-	console.log(pickedFact);
-	$("#myPopup").append(pickedFact);
-	});
+	var num=Math.round(Math.random()*(0-18)*-1);
+	var pickedFact=facts[num];
+	
+	
