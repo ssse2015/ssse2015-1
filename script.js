8529ed159014c6c9f391520a318369d631832d87
unknown <SSSE2015@E6440_LEGO1.pace.edu>
2015-07-01 20:47:11 -0400

    clickable oyster facts

---
+++ script.js
@@ -0,0 +1,31 @@
+
+var facts=[
+	"Oysters are actually a name for a few types of clams and mollusks",
+	"A group of oysters is called a bed, colony or reef",
+	"They have super strong adductor muscles to pull their shells closed when threatened",
+	"Oysters are bivalves which essentially means it’s like an open close shell connected by a ligament",
+	"Oysters are keystone species in the NY harbor and they are functionally extinct due to pollution and over harvesting",
+	"BOP has grown 11.5 million oysters in the NY harbor",
+	"Oysters reproduce when they feel a change in water temperature or the biomass of phytoplankton increases",
+	"It only takes one oyster to set off the rest of them",
+	"They reproduce by releasing a Gonad which will be either sperm or eggs into the water",
+	"An egg must be fertilized by both an egg and a sperm in order to develop",
+	"Once an egg is fertilized it drifts away from spawning site due to water currents",
+	"Oyster larvae develop for about two weeks",
+	"When oyster larvae reach that pediveliger stage(larvae with a foot), they find a hard place to attach and grow",
+	"Once they are attached they become a spat and begin to metamorphose",
+	"The spat build a shell by secreting calcium carbonate into the water column",
+	"Oysters are considered mature at one year of age",
+	"Oysters are considered adults at three years of age",
+	"Higher salinity areas mean oysters grow faster",
+	"One recycled shell can foster 20 baby oysters"
+	];
+	
+
+
+$(document).ready(function(){
+	var fact=Math.round(Math.random()*(0-18)*-1);
+	var pickedFact=facts[fact];
+	console.log(pickedFact);
+	$("#myPopup").append(pickedFact);
+	});
