41b9d0317417d15746bd331585cb275d0e4fc521
unknown <SSSE2015@E6440_LEGO1.pace.edu>
2015-07-01 21:54:36 -0400

    fixed facts

--- script.js
+++ script.js
@@ -2,28 +2,34 @@
 var facts=[
 	"Oysters are actually a name for a few types of clams and mollusks",
 	"A group of oysters is called a bed, colony or reef",
-	"They have super strong adductor muscles to pull their shells closed when threatened",
+	"Oysters have super strong adductor muscles to pull their shells closed when threatened",
 	"Oysters are bivalves which essentially means that they have an open close shell connected by a ligament",
 	"Oysters are keystone species in the NY harbor and they are functionally extinct due to pollution and over harvesting",
-	"BOP has grown 11.5 million oysters in the NY harbor",
+	"The Billion Oyster Project has grown 11.5 million oysters in the NY harbor so far and they plan to grow one billion",
 	"Oysters reproduce when they feel a change in water temperature or the biomass of phytoplankton increases",
 	"It only takes one oyster to start the reproduction process to make the rest of them join in",
 	"Oysters reproduce by releasing a Gonad which will be either sperm or eggs into the water",
-	"An egg must be fertilized by both an egg and a sperm in order to develop",
-	"Once an egg is fertilized it drifts away from spawning site due to water currents",
+	"An oyster egg must be fertilized by both an egg and a sperm in order to develop",
+	"Once an oyster egg is fertilized it drifts away from spawning site due to water currents",
 	"Oyster larvae develop for about two weeks",
 	"When oyster larvae reach that pediveliger stage(larvae with a foot), they find a hard place to attach and grow",
-	"Once they are attached they become a spat and begin to metamorphose",
+	"Once oyster larvae are attached to a hard surface they become a spat and begin to metamorphose",
 	"The spat build a shell by secreting calcium carbonate into the water column",
 	"Oysters are considered mature at one year of age",
 	"Oysters are considered adults at three years of age",
-	"Higher salinity areas mean oysters grow faster",
-	"One recycled shell can foster 20 baby oysters"
+	"Higher salinity in a certain area means that the oysters will grow faster",
+	"One recycled shell can foster 20 baby oysters",
+	"Oysters eat phytoplankton in the sea",
+	"Oysters are very useful because they clean and filter the water they dwell in",
+	"Oysters are useful to the Billion Oyster Project because they clean the New York Harbor",
+	"Many people view oysters as a part of American culture and connect them to patriotism",
+	"An oyster changes their gender at least once throughout their life",
+	"In New York, many restaurants donate their used oyster shells to the Billion Oyster Project so they can grow more oysters"
 	];
 	
 
 
-	var num=Math.round(Math.random()*(0-18)*-1);
+	var num=Math.round(Math.random()*(0-24)*-1);
 	var pickedFact=facts[num];
 	
 	
