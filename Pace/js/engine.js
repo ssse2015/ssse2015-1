f36fca0b766db21eb719a0a9a56644ab934baa2e
MrLever <mrleverx@gmail.com>
2015-07-03 09:18:44 -0400

    I actually contributed something.

--- Pace/js/engine.js
+++ Pace/js/engine.js
@@ -1,5 +1,6 @@
 var engine = {};   // create the engine object
 
+		   
 engine.width = 
 engine.outhnd = document.getElementById('output');   // handle to the output div
 engine.canvas = document.getElementById('canvas');   // handle to the canvas element
@@ -21,7 +22,22 @@ engine.map = {};
 
 engine.map.draw = function(mapData)
 {
-   /* code here */
+    var y,x;
+	var mapX = 0;
+	var mapY = 0;
+	var tile;
+	
+	engine.output('drawing map from ' + engine.viewport.x + ',' + engine.viewport.y + ' to ' +
+                 (engine.viewport.x + engine.screen.tilesX) + ',' + (engine.viewport.y + engine.screen.tilesY));
+
+    for(y=0; y<engine.screen.tilesY; y++){
+		for(x=0; x<engine.screen.tilesX; x++){
+			mapX = x+ engine.viewport.x;
+			mapY = y+ engine.viewport.y;
+			
+			tile = (mapData[mapY] && mapData[mapY][mapX]) ? mapData[mapY][mapX] : ' ';
+		}
+	}
 };
 
 
--- Pace/js/engine_old.js
+++ Pace/js/engine_old.js
@@ -1,61 +1,34 @@
 // Main Variables
+
+//canvas2d.globalAlpha = 0.5
 var oysters = 0;
-var pearls = 0;
+var pearls = 100;
+var pollution = 100;
 var grid;
-// Game Map
-// 24 columns
-// 11 rows
-var map = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-           [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-		   ];
-// Create the Overlay grid
+var xClick = 0;
+var yClick = 0;
+var timer = 0;
+/* var oySprite = new Image();
+oySprite.onload = function(){ imgReady = true; };
+oySprite.src = 'images/oy.png'; */
 
 
-// Create the canvas
+// FILL WITH DIRTY BACKGROUND PICTURE FINISH!!!!
+/* var dirtyCanvas = document.createElement("canvas");
+var dirtyCtx = dirtyCanvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 562;
+document.body.appendChild(dirtyCanvas);
+var dirtyWater = document.getElementbyId("dirtyReference");
+ */
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 512;
+canvas.height = 562;
 document.body.appendChild(canvas);
 
-// Background image
-/* var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-	bgReady = true;
-};
-bgImage.src = "images/background.png"; */
-
-
-
-// Draw everything
-/* var renderStatics = function () {
-	
-     if (bgReady) {
-		var ptrn = ctx.createPattern(bgImage, 'repeat'); // Create a pattern with this image, and set it to "repeat".
-	    ctx.fillStyle = ptrn;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-	}
-	// Black Bar
-	
-
-	
-	//grid
-	
-}
-renderStatics(); */
 
 function renderDynamics(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "rgb(0, 0, 0)";
 	ctx.fillRect(0,0,canvas.width,52);	
 	ctx.fillStyle = "rgb(250, 250, 250)";
@@ -63,9 +36,58 @@ function renderDynamics(){
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("Oysters: " + oysters, 16, 8);
-	ctx.fillText("Pearls: " + pearls, canvas.width - 125, 8);
+	ctx.fillText("Pearls: " + pearls, canvas.width - 145, 8);
+}
+/* var oyImage = new Image(); */
+function addOy(x, y){
+	temp = new Oy(xClick, yClick, 0, 1);
+	placedOysters.push(temp)
+	console.log(placedOysters.length);
+	var oyImage = document.getElementById("reference");
+	ctx.drawImage(oyImage, x, y, 32, 32);	
+	/* oyImage.style.display = "initial"; */
+	/* console.log(x, y); */
+	/* ctx.drawImage(oyImage, x, y, 32, 32); */
+	console.log("I'M DRAWN TO IT")
+}
+// Game Objects
+var placedOysters = new Array();
+function Oy(x, y, age, prod){
+	this.x = x;
+	this.y = y;
+	this.age = age;
+	this.prod = prod;
+	dy = y +32;
+	dx = x + 32;
+	
 }
 
+
+canvas.addEventListener("mousedown", getPosition, false);
+canvas.addEventListener("mousedown", validateClick, false);
+
+function getPosition(event)
+{
+  xClick = event.x;
+  yClick = event.y;
+
+  console.log("x:" + xClick + " y:" + yClick);
+}
+function checkClick(){
+	if (yClick < 52 || pearls - 10 < 0)
+		return false;
+	else
+		return true;
+}
+function validateClick(){
+	for(i = 0; i < placedOysters.length; i++){
+		/* console.log(placedOysters[i].x) */
+		if((xClick < placedOysters[i].x + 32)&&(yClick < placedOysters[i].y + 32)
+			&& (xClick >= placedOysters[i].x) && (yClick >= placedOysters[i].y)){
+				pearls++;
+			}
+	}
+}
 /* var tileWidth = window.innerWidth / 24;
 var tileHeight = tileWidth;
 function renderGrid(){
@@ -81,10 +103,22 @@ function renderGrid(){
 }
 renderGrid(); */
 var update = function (modifier){
-	//map
-	//counters
-	//oyster's stage
+	var canPlace = checkClick();
+	if (canPlace && (pearls > 0)){
+		addOy(xClick, yClick);
+		pearls-= 10;
+		oysters++;
+	}
+	if(oysters > 0 && timer == 1800){
+		pearls += oysters;
+	}
 	
+
+	
+}
+function resetClick(){
+	xClick = 0;
+	yClick = 0;
 }
 var main = function () {
 	var now = Date.now();
@@ -94,15 +128,22 @@ var main = function () {
 	
 	renderDynamics();
 	then = now;
-	console.log('alive')
-	// Request to do this again ASAP
+	console.log('beep');
+	resetClick();
+	if(timer < 1800){
+		timer++;
+	}
+	else{
+		timer = 0;
+	}
 	requestAnimationFrame(main);
+	
 };
 
 // Cross-browser support for requestAnimationFrame
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-// Let's play this game!
+// run the ******* game
 var then = Date.now();
 main();
